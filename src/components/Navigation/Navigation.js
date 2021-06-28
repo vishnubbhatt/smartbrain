cdeb13886b0ee454ec5bf1ed27153c6c6311d40c
Vishnu Bhatt <vishnubbhatt@gmail.com>
2021-06-28 16:18:53 +1000

    Initial Commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+        <div>
+            <nav>
+                <p>
+                    Sign Out
+                </p>
+            </nav>
+            
+        </div>
+    )
+}
